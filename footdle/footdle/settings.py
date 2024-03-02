c0e68d748910b6bbe084a85cf07bd73371da2f7f
1Cezzo <emiljohnzen@gmail.com>
2024-03-02 13:38:33 +0100

    heroku

--- footdle/footdle/settings.py
+++ footdle/footdle/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 import os
 from dotenv import load_dotenv
 
@@ -27,7 +29,7 @@ SECRET_KEY = 'django-insecure-ot&d#qwr95hiqogj7y4pdpp+ic+$%b*-gk85beg7esl)ojasr(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -132,6 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

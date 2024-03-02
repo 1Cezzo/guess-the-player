eed70325a8f1d8573daef6ec0b0dc7253f766e2f
1Cezzo <emiljohnzen@gmail.com>
2024-03-02 01:08:46 +0100

    you can now send requests from frontend to backend

--- footdle/footdle/settings.py
+++ footdle/footdle/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'app.player',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'footdle.urls'
--- guess-the-player/src/app/premier-league/page.tsx
+++ guess-the-player/src/app/premier-league/page.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const PremierLeaguePage: React.FC = () => {
+  const [players, setPlayers] = useState<any[]>([]);
+  const API_BASE_URL = 'http://localhost:8000';
+
+  const endpointUrl = `${API_BASE_URL}/players/`;
+
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      try {
+        const response = await axios.get(endpointUrl);
+        console.log('Players:', response.data);
+        setPlayers(response.data);
+      } catch (error) {
+        console.error('Error fetching players:', error);
+      }
+    };
+
+    fetchPlayers();
+  }, []);
+
   return (
     <div className="flex flex-col h-screen items-center mt-auto" style={{ marginTop: '15vh' }}>
-      <div className="bg-green-200 border border-gray-400 p-5 rounded shadow-md rounded-lg w-[350px]">
+      <div className="bg-white bg-green-200 shadow-md rounded-lg p-4 w-[350px] mb-4">
         <p className="text-lg font-bold">Guess the player of the day!</p>
         <p className="text-gray-600">Type the name of any football player</p>
       </div>
       <input
         type="text"
-        className="border border-gray-400 p-2 mt-10 shadow-md rounded-md w-[350px]"
+        className="border border-gray-400 p-2 shadow-md rounded-md w-[350px] mb-4"
         placeholder="Enter player's name"
       />
+      <div>
+        <h2 className="text-xl font-bold mb-2">Players:</h2>
+        <ul>
+          {players.map(player => (
+            <li key={player.id}>{player.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
